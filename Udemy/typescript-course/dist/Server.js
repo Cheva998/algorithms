7c64b50556f93d135be0ab167e2023fd126b8d04
Sebastian Duque <sduquet998@gmail.com>
2024-06-11 15:36:19 +0000

    implements interface and class  of server

--- Udemy/typescript-course/dist/Server.js
+++ Udemy/typescript-course/dist/Server.js
@@ -4,7 +4,44 @@ class BaseServer {
         this.port = port;
         this.address = address;
     }
+    ;
+}
+;
+class Server extends BaseServer {
+    constructor(port, address) {
+        super(port, address);
+        console.log('Instatiating server');
+    }
+    ;
     startServer() {
         console.log(`Starting server at: ${this.address}:${this.port}`);
     }
+    ;
+    stopServer() {
+        console.log(`Stoping server: ${this.address}`);
+    }
+    ;
 }
+;
+class InterfacedServer {
+    constructor(port, address) {
+        this.port = port;
+        this.address = address;
+    }
+    ;
+    startServer() {
+        console.log(`Starting interfaced server @${this.address}:${this.port}`);
+    }
+    ;
+    stopServer() {
+        console.log(`Stoping interfaced server ... ${this.address}`);
+    }
+    ;
+}
+;
+const someServer = new Server(8080, 'localhost');
+someServer.startServer();
+someServer.stopServer();
+const iServer = new InterfacedServer(8090, 'localhost');
+iServer.startServer();
+iServer.stopServer();
--- Udemy/typescript-course/src/Server.ts
+++ Udemy/typescript-course/src/Server.ts
@@ -1,7 +1,5 @@
 
 
-
-
 abstract class BaseServer {
     protected port: number;
     protected address: string;
@@ -9,13 +7,56 @@ abstract class BaseServer {
     constructor(port: number, address: string) {
         this.port = port;
         this.address = address;
-    }
+    };
 
-    startServer() {
-        console.log(`Starting server at: ${this.address}:${this.port}`)
-    }
+    abstract startServer(): void;
 
-    abstract stopServer(): void
-    
+    abstract stopServer(): void;
+};
+
+class Server extends BaseServer {
+    constructor(port: number, address: string) {
+        super(port, address);
+        console.log('Instatiating server');
+    };
+
+    startServer(): void {
+        console.log(`Starting server at: ${this.address}:${this.port}`);        
+    };
+
+    stopServer(): void {
+        console.log(`Stoping server: ${this.address}`);
+    };
+};
+
+
+interface IServer {
+    startServer(): void
+    stopServer(): void
 }
 
+class InterfacedServer implements IServer {
+    public port: number;
+    public address: string;
+
+    constructor(port: number, address: string) {
+        this.port = port;
+        this.address = address;
+    };
+
+    startServer(): void {
+        console.log(`Starting interfaced server @${this.address}:${this.port}`);
+    };
+
+    stopServer(): void {
+        console.log(`Stoping interfaced server ... ${this.address}`)
+    };
+};
+
+const someServer = new Server(8080, 'localhost');
+someServer.startServer();
+someServer.stopServer();
+
+const iServer: IServer = new InterfacedServer(8090, 'localhost');
+iServer.startServer();
+iServer.stopServer();
