e3ebb74171b1e433196bff22265dbcb98fa0bcdf
Sebastian Duque <sduquet998@gmail.com>
2024-06-13 22:58:28 +0000

    added new course on backend with typescript and node

---
+++ Udemy/ts-node-server/src/Launcher.ts
@@ -0,0 +1,16 @@
+import { Server } from "./Server";
+
+
+
+
+class Launcher {
+
+    private server: Server = new Server();
+
+    public launchApp() {
+        this.server.startServer();
+    }
+
+}
+
+new Launcher().launchApp();
