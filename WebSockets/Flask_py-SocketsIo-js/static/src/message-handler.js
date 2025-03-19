8d1f08ed4a4cc95231a9f85af0dae8571f5c26ed
Sebastian Duque <sduquet998@gmail.com>
2025-03-19 18:54:58 +0000

    remove file

--- WebSockets/Flask_py-SocketsIo-js/static/src/message-handler.js
+++
@@ -1,6 +0,0 @@
-export function createMessageElement(message) {
-    const messageElement = document.createElement('div');
-    messageElement.className = message;
-    messageElement.innerHTML = `<strong>${data.username}:</strong> ${data.message}`;
-    return messageElement
-}
